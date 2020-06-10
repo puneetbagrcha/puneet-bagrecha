90cb66a5c0c1660277d58c0af8b95eea25bd436e
Puneet Bagrecha <bagrechapuneet01@gmail.com>
2020-06-11 00:02:35 +0530

    changed app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+title{color:green}
+
+h1{ color:white;
+	 text-align:center;
+	 background-color:black;
+	 padding:30px;}
+hr{
+	border:2px solid black;
+	width: 50%;
+}
+
+p{font-family:Arial;}
+
+li{display: inline;
+   margin: 100px;
+   text-align:center;}
+
+li a{color:white;
+     text-decoration-style:none;}
+
+header{background-color:black;
+       padding: 10px}
+
+li a:hover{color:red;
+         text-decoration:underline:none;}
+
+footer
+      {background-color:black;
+	   color:white;
+       text-align:center;
+       padding-right:60px;}
+
+footer:hover {color:green;}
+
+.special_para
+{text-align:justify-all;
+font-size: 20px;}
+
+p {margin-left:50px;
+   margin-right: 50px;}
+
+div :hover {
+	color: green
+}   
+.img-container
+{margin-left:50px;
+margin-right:50px; 
+margin-bottom: 50px;}
